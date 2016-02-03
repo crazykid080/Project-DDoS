59ceee24f1f3482b9ad5eb28f996f79e207fa31a
crazykid080 <orillyt080@gmail.com>
2016-02-03 09:37:11 -0800

    Create DriverConstructor.java
    Added basic design, will be expanded and edited heavily.
    
    ADDED FROM TABLET

---
+++ HP like game/src/com/crazykid080/main/constructors/DriverConstructor.java
@@ -0,0 +1,24 @@
+package com.crazykid080.main.constructors;
+
+public class DriverConstructor{
+
+private double version = 0;
+
+public DriverConstructor(DriverTypes type){
+
+switch(type){
+// TODO: Implement all driver types.
+}
+
+
+}
+
+public DriverConstructor(DriverTypes type, double version){
+
+switch(type){
+//TODO: Implement all driver types
+}
+
+}
+
+}
