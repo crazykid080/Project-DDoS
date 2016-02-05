1a939970372922d3f90d496aaec535409fe345ac
crazykid080 <orillyt080@gmail.com>
2016-02-05 10:10:54 -0800

    Create LogFrame.java
    This will be the log frame.

---
+++ HP like game/src/com/crazykid080/main/UI/LogFrame.java
@@ -0,0 +1,23 @@
+package com.crazykid080.main.UI;
+
+import javax.swing.*;
+
+
+public class LogFrame extends JFrame{
+
+private JTextArea textArea1 = new JTextArea();
+private ArrayList<String> Logs = new ArrayList<String>();
+
+public LogFrame(){
+
+}
+
+//getters and setters?
+
+//public methods
+
+public void addLog(String log){
+
+}
+
+}
