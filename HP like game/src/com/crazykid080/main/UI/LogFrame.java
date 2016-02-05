7b146f4b8f020941bea63b7cf924611bf646ad28
crazykid080 <orillyt080@gmail.com>
2016-02-05 11:09:39 -0800

    Update LogFrame.java
    Added some functional code.

--- HP like game/src/com/crazykid080/main/UI/LogFrame.java
+++ HP like game/src/com/crazykid080/main/UI/LogFrame.java
@@ -5,10 +5,21 @@ import javax.swing.*;
 
 public class LogFrame extends JFrame{
 
+private JPanel panel1 = new JPanel();
 private JTextArea textArea1 = new JTextArea();
 private ArrayList<String> Logs = new ArrayList<String>();
 
 public LogFrame(){
+
+//normal code here
+
+//functional code here
+
+textArea1.setEditable(false);
+
+panel1.add(textArea1);
+
+this.add(panel1);
 
 }
 
@@ -17,7 +28,7 @@ public LogFrame(){
 //public methods
 
 public void addLog(String log){
-
+textArea1.setText(textArea1.getText() + log + "\n");
 }
 
 }
