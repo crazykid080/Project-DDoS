954d9851717422aaa8e97f0551aa8bbdfb857e4c
crazykid080 <orillyt080@gmail.com>
2016-02-10 12:43:26 -0800

    Update ProcessActions.java
    Created more methods and write some basic code for it, including comments to tell what is needed

--- HP like game/src/com/crazykid080/main/ProcessActions.java
+++ HP like game/src/com/crazykid080/main/ProcessActions.java
@@ -10,9 +10,19 @@ public void onProcessKill(ProcessConstructor obj, boolean log){
 obj.setRunning(false);
 
 if(log == true){
+Server s = obj.getServer();
 //log.write(originip + " killed process " + process);
 }
 
-}
+}  //method end PLEASE REMOVE THIS AFTER YOU REFORMAT THIS
+
+public void onProcessStart(ProcessConstructor obj, boolean log){
+
+}//method end REMOVE COMMENT
+
+public void onInstallStart(/*NEED TO CREATE THE FILES*/ int mil){
+//new timer(time, file);
+//
+}  //method end DELETE THIS COMMENT
 
 }
