add67a2eb50599a77739737e9cf3854ef9b1a0bf
crazykid080 <orillyt080@gmail.com>
2016-02-16 10:07:27 -0800

    Create FileConstructor.java
    Created the basic structure of the file constructor.

---
+++ HP like game/src/com/crazykid080/main/FileConstructor.java
@@ -0,0 +1,17 @@
+package com.crazykid080.main;
+
+//TODO: move this to a new package
+public class FileConstructor{
+
+//TODO: add file ID controller
+//TODO: add file type enum
+String publicName = "ERROR!!!";
+private Server parent = null;
+protected double burnout = -1;
+public double version = -1;
+
+public FileConstructor(){
+
+}
+
+}
