581b79a391e39e3443fde9b9197a05b1cba1e4b4
crazykid080 <orillyt080@gmail.com>
2016-02-17 12:41:15 -0800

    Update FileConstructor.java
    Created code for the constructor and a new method for research.

--- HP like game/src/com/crazykid080/main/FileConstructor.java
+++ HP like game/src/com/crazykid080/main/FileConstructor.java
@@ -10,8 +10,30 @@ private Server parent = null;
 protected double burnout = -1;
 public double version = -1;
 
-public FileConstructor(){
+public FileConstructor(ProcessTypes type, double vers, Server parent){
+
+this.burnout = 0;
+this.version = vers;
+this.parent = parent;
+
+publicName = type + " (" + burnout + " ) " + version;
 
 }
+
+public void doResearch(int hours){
+
+if(version >= 10){
+
+double addition = hours * .01;
+
+}else{
+
+double addition = hours * .001 / 2;
+
+}
+
+ConsoleControl.write("research will add; " + addition, LogLevels.Info);
+
+}//method end
 
 }
