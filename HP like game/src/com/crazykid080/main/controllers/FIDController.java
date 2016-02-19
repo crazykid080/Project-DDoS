d9c3ceddfb3f828c7fcafac1f13c4125288e0028
crazykid080 <orillyt080@gmail.com>
2016-02-19 08:45:48 -0800

    Create FIDController.java
    Created base code for the new class.

---
+++ HP like game/src/com/crazykid080/main/controllers/FIDController.java
@@ -0,0 +1,14 @@
+package com.crazykid080.main.controllers;
+
+public class FIDController{
+
+protected ArrayList<Object> files = new ArrayList<Object>();
+
+public int registerFile(Object hash){
+files.add(hash);
+//get index + one
+//return
+return -1;
+}
+
+}
