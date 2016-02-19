04d8a2c34cd40e1c606cc8f3fd449e250b782ce9
crazykid080 <orillyt080@gmail.com>
2016-02-19 10:48:18 -0800

    Update FIDController.java
    Added more code, finished some of the pieces.

--- HP like game/src/com/crazykid080/main/controllers/FIDController.java
+++ HP like game/src/com/crazykid080/main/controllers/FIDController.java
@@ -5,10 +5,22 @@ public class FIDController{
 protected ArrayList<Object> files = new ArrayList<Object>();
 
 public int registerFile(Object hash){
-files.add(hash);
-//get index + one
-//return
-return -1;
+
+if(files.contains(hash){
+return files.indexOf(hash);
 }
+
+files.add(hash);
+return files.indexOf(hash);
+
+}
+
+public void unregisterFile(Object hash){
+
+//this is probably the wrong method
+files.replace(hash, null);
+
+}
+
 
 }
