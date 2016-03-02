6fb5813101cf5cbd3a2283c1c14590d061221ffd
crazykid080 <orillyt080@gmail.com>
2016-03-02 10:06:07 -0800

    Update DriverFrame.java
    Added slight additions.

--- HP like game/src/com/crazykid080/main/UI/DriverFrame.java
+++ HP like game/src/com/crazykid080/main/UI/DriverFrame.java
@@ -2,7 +2,25 @@ package com.crazykid080.main.UI;
 
 public class DriverFrame extends JFrame{
 
+private JPanel panel1 = new JPanel();
+
+
+//---------- Swing components above, non swing below
+
+protected Server parent = null;
+
 public DriverFrame(){
+
+}
+
+public DriverFrame(Server parent){
+
+//TODO: add code from other UI that has the variable size
+
+this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+this.setVisible(true);
+
+this.parent = parent;
 
 }
 
