b58771ca0de7ddf0882a26c79c8f320aa55ce64e
crazykid080 <orillyt080@gmail.com>
2016-03-22 16:02:44 -0700

    Create FactionNames.java
    Now that I know how to create new folders, some development can be done.

---
+++ HP like game/src/com/crazykid080/main/faction/FactionNames.java
@@ -0,0 +1,17 @@
+package com.crazykid080.main.faction;
+
+public enum FactionNames{
+
+The_Guardians,
+The_Militia,
+The_Watchers,
+
+//Anti-Gov up
+
+//Pro-Gov down
+
+The_Intelligence,
+The_Commanders,
+The_Breachers
+
+}
