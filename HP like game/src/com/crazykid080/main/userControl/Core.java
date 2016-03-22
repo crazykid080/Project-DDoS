72f44f15a8162f577494cb6106855146155363e0
crazykid080 <orillyt080@gmail.com>
2016-03-22 16:08:09 -0700

    Update Core.java
    Was able to give some formatting. Added one extra line

--- HP like game/src/com/crazykid080/main/userControl/Core.java
+++ HP like game/src/com/crazykid080/main/userControl/Core.java
@@ -5,34 +5,32 @@ public int subCoreNum =-1;
 protected boolean isMainCore = false;
 
 public class Core extends Server{
-
-public Core(){
-
-isMainCore = true;
-subCoreNum = 0;
-
-IPAddress = IPController.obtainIP(this);
-
-Core core1 = new Core(this);
-Core core2 = new Core(this);
-
-}
-
-public Core(Core parent){
-
-int num = parent.getsubCoreNum();
-subCoreNum = num + 1;
-
-IPAddress = IPController.obtainIP(this);
-
-}
+  
+  public Core(){
+    
+    isMainCore = true;
+    subCoreNum = 0;
+    IPAddress = IPController.obtainIP(this);
+    Core core1 = new Core(this);
+    Core core2 = new Core(this);
+    Core core3 = new Core(this);
+    
+  }
+  
+  public Core(Core parent){
+    
+    int num = parent.getsubCoreNum();
+    subCoreNum = num + 1;
+    IPAddress = IPController.obtainIP(this);
+    
+  }
 
 
 
 //getters and setters below
 
 public int getsubCoreNum(){
-return subCoreNum;
+  return subCoreNum;
 }
 
 }
