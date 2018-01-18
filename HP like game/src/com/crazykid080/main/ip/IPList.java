13478e74fdeb379912145cc2f6bfd69b30a0e37c
Timothy O'Reilly <10173541+crazykid080@users.noreply.github.com>
2018-01-18 13:12:28 -0800

    Removed unecessary if statements in IPList

--- HP like game/src/com/crazykid080/main/ip/IPList.java
+++ HP like game/src/com/crazykid080/main/ip/IPList.java
@@ -6,14 +6,6 @@ public class IPList {
 	private static ArrayList<String> RegisteredIPs = new ArrayList<String>();
 
 	public static boolean registerIP(String IP){
-		if(RegisteredIPs == null) {
-			RegisteredIPs.add(IP);
-			return true;
-		}
-		if(RegisteredIPs.isEmpty()) {
-			RegisteredIPs.add(IP);
-			return true;
-		}
 		if(RegisteredIPs.contains(IP)) {
 			return false;
 		}else {
